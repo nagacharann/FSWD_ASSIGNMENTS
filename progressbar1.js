8c2f939f8261c07accd806ab7a71110e5c602148
Naga charan <cherryiloveu464@gmail.com>
2023-04-18 12:10:09 +0530

    progressbar js

--- progressbar1.js
+++ progressbar1.js
@@ -9,7 +9,9 @@ const todo = [{
 const todoContainer = document.querySelector('ul');
 const input = document.querySelector('input');
 const content = document.querySelector('#content');
+
 const progressBar = document.querySelector('progress')
+
 
 const contentHeight = content.clientHeight
 
@@ -18,5 +20,5 @@ document.addEventListener('scroll',(event)=>{
     const percentage = scroll / (contentHeight - window.innerHeight)
     console.log(percentage*100)
     progressBar.value = (percentage*100).toFixed(0)
-    
+
 })
