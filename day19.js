9b0d5bcaffcd9d2244a95ec2505c72a46b9e5408
Naga charan <cherryiloveu464@gmail.com>
2023-04-26 15:50:52 +0530

    rock paper scissor

---
+++ day19.js
@@ -0,0 +1,33 @@
+//   fetch('https://jsonplaceholder.typicode.com/todos')
+// .then(data=>data.json())
+// .then(result=>{
+//   console.log(result)
+// }).catch(error=>{
+//   console.log(error)
+// })
+
+
+
+  const getTodos = async () => {
+    try {
+      console.log(1);
+  
+      await new Promise((resolve) => {
+        setTimeout(() => resolve(), 4000);
+      })
+  
+      console.log(2)
+  
+       new Promise((resolve) => {
+        setTimeout(() => resolve(), 9000);
+      })
+  
+      console.log(3)
+  
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  getTodos();
+  console.log("hello");
